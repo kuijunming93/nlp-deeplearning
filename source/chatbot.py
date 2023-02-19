766597468c4ffe98baf204ec1d68e5d0c3dd8007
kuijunming93 <kuijunming93@gmail.com>
2023-02-20 01:40:25 +0800

    Added comment for steps of NLP phase

--- source/chatbot.py
+++ source/chatbot.py
@@ -4,6 +4,8 @@ import re
 import time
 import utilities
 
+# DATA PROCESSING
+# reading raw files into memory for processing
 memoryLine = {}
 with open('source/resources/movie_lines.txt', encoding='utf-8', errors='ignore') as f:
     _movieLines = f.read().split("\n")
@@ -19,6 +21,7 @@ with open('./source/resources/movie_conversations.txt', encoding='utf-8', errors
         _element = _conversation.split(" +++$+++ ")[-1][1:-1].replace("'","").replace(" ","")
         memoryConversation.append(_element.split(","))
 
+# cleaning up raw data of punctuations and lower casing
 clean_questions = []
 clean_answers = []
 for conversation in memoryConversation:
@@ -26,22 +29,28 @@ for conversation in memoryConversation:
         clean_questions.append(utilities.cleanText(memoryLine[conversation[i]]))
         clean_answers.append(utilities.cleanText(memoryLine[conversation[i+1]]))
 
+# consolidating vocabularies from cleaned data
 total = []
 total.append(clean_questions)
 total.append(clean_answers)
 vocabulary = utilities.consolidateWordCount(total)
 
+# tokenizing vocabularies to index
 questionsWords2Int = utilities.convertVocabToInt(vocabulary, 20)
 answersWords2Int = utilities.convertVocabToInt(vocabulary, 20)
 
+# reverse tokenizing dictionary
 answersInt2Words = {v:k for k, v in answersWords2Int.items()}
 
+# adding EOS for all answers
 for i in range(len(clean_answers)):
     clean_answers[i] += '<EOS>'
 
+# tokenizing all clean data into indices
 clean_questions_intoInt = utilities.convertSentencesToInt(clean_questions, questionsWords2Int)
 clean_answers_intoInt = utilities.convertSentencesToInt(clean_answers, answersWords2Int)
 
+# sorting out of tokenized clean data to simplify training process
 sorted_clean_questions = []
 sorted_clean_answers = []
 for length in range(1, 25 + 1):
